9be549ec9969def5dff047c55e05d4d050b6ff25
Romain Lebbadi-Breteau <romain@lebbadi.fr>
2019-09-05 21:36:18 -0400

    add various messages and commands

--- client.py
+++ client.py
@@ -1,17 +1,42 @@
 #!/usr/bin/python3
 
 import socket               # Import socket module
+import re
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12347                # Reserve a port for your service.
+#host = input("Host: ")
+host = "127.0.0.1"
+port = 1234                # Reserve a port for your service.
 
 s.connect((host, port))
+print("Connected to", s.getpeername())
 
 while True:
     text = input("Votre message: ")
-    if text == "quit": break
-    newText = bytearray(text, "utf-8")
-    s.sendall(newText)
+    regex = re.search("^/([a-z]*)( ([a-zA-Z0-9]*))?$", text)
+
+    if(regex):
+        command = regex.group(1)
+        arg = regex.group(3)
+
+        if(command == "quit"):
+            break
+        elif(command == "nick"):
+            if(arg == None):
+                print("No nickname provided")
+            else:
+                s.send(bytearray("/nick " + arg, "utf-8"))
+                print("Nickname changed")
+        elif(command == "help"):
+            print("Available Commands:")
+            print("/quit: Quit the app")
+            print("/help: Show this page")
+            print("/nick <nickname>: Change nickname")
+        else:
+            print("Unknown Command")
+
+    else:
+        text = bytearray(text, "utf-8")
+        s.send(text)
 #s.sendall("Je suis content")
 s.close()                     # Close the socket when done
--- server.py
+++ server.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 
 import socket
+import re
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12347                 # Reserve a port for your service.
+host = "0.0.0.0" # Get local machine name
+port = 1234                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+print("Listening on",host,"on port",port)
 
 s.listen(1)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print('Got connection from', addr)
+   username = addr
    while True:
        text = c.recv(1024)
        if not text: break
        text = text.decode('utf-8', 'ignore')
-       print(text)
+       regex = re.search("^/([a-z]*)( ([a-zA-Z0-9]*))?$", text)
+       if(regex):
+           command = regex.group(1)
+           arg = regex.group(3)
+           if(command == "nick"):
+               if(arg != None):
+                   print(username, "changed username to", arg)
+                   username = arg
+       else:
+           print(username, ":", text)
    c.close()
-   print("client disconnected")
+   print("client", username, "disconnected")
