12acf0281ec6368ab01119ca73f117498dede081
Romain Lebbadi-Breteau <romain@lebbadi.fr>
2020-02-13 20:14:21 -0400

    add line between 2 functions

--- backend.py
+++ backend.py
@@ -61,6 +61,7 @@ def keyToJson(key):
 def stripPrivateKey(key):
     return dict((k, v) for k, v in key.items() if k != "d")
 
+
 def keyToBase64(key):
     return base64.b64encode(keyToJson(stripPrivateKey(key)).encode())
 
